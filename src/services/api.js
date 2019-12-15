95c84ad0a015b47ae780ae6a11827851e8ca6b04
Shaelyn <shaelynsun@qq.com>
2019-12-15 00:49:33 +0000

    readme pic wrong

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     //    baseURL: 'http://localhost:3000/'
-    baseURL: 'https://dst-staging-api.herokuapp.com/'
+    baseURL: 'https://dst-prod-api.herokuapp.com/'
   })
 }
